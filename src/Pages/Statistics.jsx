8f4016ed5433219412162aac5a3ec672ba2977eb
mohadebDev <maxff00789@gmail.com>
2023-09-25 22:28:47 +0600

    added statistics section with dynamic piechart

--- src/Pages/Statistics.jsx
+++ src/Pages/Statistics.jsx
@@ -1,8 +1,29 @@
-
+import { useEffect, useState, useContext } from "react";
+import { AllDonation } from "../Root/Root";
+import Chart from "../components/PieChart";
 
 export default function Statistics() {
+
+  const allDonation = useContext(AllDonation);
+
+  const [ totalDonation ,setTotalDonation ] = useState(0);
+  const [ yourDonation ,setYourDonation ] = useState(0);
+
+  useEffect(()=>{
+
+    setTotalDonation(allDonation.reduce((preValue, currentValue)=> preValue + currentValue.price ,0 ))
+
+    const storedDonationIds = JSON.parse(localStorage.getItem('donationIds'));
+    if(storedDonationIds){
+     const foundDonation = allDonation.filter(donation => storedDonationIds.includes(donation.id));
+     setYourDonation(foundDonation.reduce((preValue, currentValue)=> preValue + currentValue.price ,0 ))
+    }
+
+  },[])
+
   return (
-    <div>
-    </div>
+    <>
+      <Chart totalDonation={totalDonation} yourDonation={yourDonation} />
+    </>
   )
 }
